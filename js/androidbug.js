224ec2e3a1eb1fd494fa5a822a6e5e9b214e9232
Simon Buysse <buysse.simon@gmail.com>
2017-06-21 01:58:46 +0200

    Add message debug

--- js/androidbug.js
+++ js/androidbug.js
@@ -1,6 +1,8 @@
 window.addEventListener("resize", function() {
   if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+      console.log("shalalalala");
      window.setTimeout(function() {
+      console.log("shalalalala");
         document.activeElement.scrollIntoViewIfNeeded();
      },0);
   }
